a1a1f18b4444889ae4db215f0620e581c83d3b3f
Hasuka <hasukapyon@163.com>
2019-04-03 23:54:13 +0800

    Update at 2019.4.3

---
+++ precache-manifest.9ffbd2c6b25f0207b6c05d1ba9afd4d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mjpoint/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a34d1e8519d364b38109",
+    "url": "/mjpoint/static/js/runtime~main.a34d1e85.js"
+  },
+  {
+    "revision": "e5f88f75c651eb957e24",
+    "url": "/mjpoint/static/js/main.e5f88f75.chunk.js"
+  },
+  {
+    "revision": "c61637969e600f7f059f",
+    "url": "/mjpoint/static/js/2.c6163796.chunk.js"
+  },
+  {
+    "revision": "e5f88f75c651eb957e24",
+    "url": "/mjpoint/static/css/main.7221bbd7.chunk.css"
+  },
+  {
+    "revision": "f7e97e6a1c53d598954a7dbfe40e2df9",
+    "url": "/mjpoint/index.html"
+  }
+];
